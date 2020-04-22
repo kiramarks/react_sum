3bcfae0af393c21cef4f17b36b9198e31c19d2d8
Kira Marks <serafita22@gmail.com>
2020-04-22 16:23:09 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0be2a4d51bf87742479c1b1baf41cd4d.js"
+  "./precache-manifest.226173020dcfafc0c0f673d9ee0d8362.js"
 );
 
 self.addEventListener('message', (event) => {
